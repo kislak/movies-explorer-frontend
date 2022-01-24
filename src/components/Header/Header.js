c906c00cbb47e322bf4111109fff5edd1cc8cab2
serge <kislak7@gmail.com>
2022-01-25 00:39:16 +0300

    cleaup header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,32 +6,50 @@ import { useNavigate } from "react-router-dom/index";
 function Header(props) {
   const navigate = useNavigate();
   return (
-    <div>
-      <header className="header">
-        <div className="header__nav">
-          <div className="header__logo">
-          </div>
-          <div className="header__link" onClick={() => navigate('/movies') }>
-            Фильмы
-          </div>
-          <div className="header__link" onClick={() => navigate('/saved-movies') }>
-            Сохранённые фильмы
-          </div>
+    <header className="header">
+      <section className="header__nav">
+        <div
+          className="header__logo"
+          onClick={() => navigate('/')}
+        >
         </div>
+        <a
+          className="header__link"
+          onClick={() => navigate('/movies') }
+        >
+          Фильмы
+        </a>
+        <a
+          className="header__link"
+          onClick={() => navigate('/saved-movies') }
+        >
+          Сохранённые фильмы
+        </a>
+      </section>
 
-        <div className="header__nav">
-          <div className="header__link" onClick={() => navigate('/signup') }>
-            Регистрация
-          </div>
-          <div className="header__link" onClick={() => navigate('/signin') }>
-            Войти
-          </div>
-          <div className="header__link" onClick={() => navigate('/profile') }>
-            Аккаунт
-          </div>
-        </div>
-      </header>
-    </div>
+      <section className="header__nav">
+        <a
+          className="header__link"
+          onClick={() => navigate('/signup') }
+        >
+          Регистрация
+        </a>
+        <button
+          className="header__button header__button_blue"
+          onClick={() => navigate('/signin') }
+          type="button"
+        >
+          Войти
+        </button>
+        <button
+          className="header__button header__button_profile"
+          onClick={() => navigate('/profile') }
+          type="button"
+        >
+          Аккаунт
+        </button>
+      </section>
+    </header>
   )
 }
 
