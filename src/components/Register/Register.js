729ae2f682d3fcd59ce167375e5a1f5a995729d8
Serge Kislak <kislak7@gmail.com>
2022-01-26 21:53:36 +0300

    add signup

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,70 +4,72 @@ import Header from "../Header/Header";
 function Register(props) {
   return (
     <section className="register">
-      <Header />
-      <h1 className="register__title">
-        Добро пожаловать!
-      </h1>
+      <Header headerOnly={true} />
+      <section className="register__container">
+        <h1 className="register__title">
+          Добро пожаловать!
+        </h1>
 
-      <form className="register__form" name="register">
-        <label
-          htmlFor="register__input-name"
-          className="register__input-label"
-        >
-          Имя
-        </label>
+        <form className="register__form" name="register">
+          <label
+            htmlFor="register__input-name"
+            className="register__input-label"
+          >
+            Имя
+          </label>
 
-        <input
-          id="register__input-name"
-          className="register__name"
-          type="text"
-          name="name"
-          autoComplete="off"
-          required
-        />
+          <input
+            id="register__input-name"
+            className="register__input"
+            type="text"
+            name="name"
+            autoComplete="off"
+            required
+          />
 
-        <label
-          htmlFor="register__input-name"
-          className="register__input-label"
-        >
-          E-mail
-        </label>
+          <label
+            htmlFor="register__input-name"
+            className="register__input-label"
+          >
+            E-mail
+          </label>
 
-        <input
-          id="register__input-email"
-          className="register__input"
-          type="text"
-          name="email"
-          autoComplete="off"
-          required
-        />
+          <input
+            id="register__input-email"
+            className="register__input"
+            type="text"
+            name="email"
+            autoComplete="off"
+            required
+          />
 
-        <label
-          htmlFor="register__input-name"
-          className="register__input-label"
-        >
-          Пароль
-        </label>
+          <label
+            htmlFor="register__input-name"
+            className="register__input-label"
+          >
+            Пароль
+          </label>
 
-        <input
-          id="register__input-password"
-          className="register__input"
-          type="password"
-          name="password"
-          autoComplete="off"
-          required
-        />
-        <p register__error>
-          Что-то пошло не так...
-        </p>
+          <input
+            id="register__input-password"
+            className="register__input register__input_error"
+            type="password"
+            name="password"
+            autoComplete="off"
+            required
+          />
+          <p className="register__error-text">
+            Что-то пошло не так...
+          </p>
 
-        <button className="register__submit" type="submit">
-          Зарегистрироваться
-        </button>
-        <div className="register__ref" >
-          Уже зарегистрированы? <a className="register__link" href="#" >Войти</a>
-        </div>
-      </form>
+          <button className="register__submit" type="submit">
+            Зарегистрироваться
+          </button>
+          <div className="register__ref" >
+            Уже зарегистрированы? <a className="register__link" href="#" >Войти</a>
+          </div>
+        </form>
+      </section>
     </section>
   )
 }
