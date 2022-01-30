bf5bce6b6a46e2fc12972d57349200941e4a5998
serge <kislak7@gmail.com>
2022-01-30 20:08:26 +0300

    menu tmp

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,53 +7,101 @@ function Navigation(props) {
   const { pathname } = useLocation();
   const loggedin = ["/movies", "/saved-movies"].includes(pathname)
 
-  return (
-    <>
-      {loggedin &&
-        <nav className="navigation">
-          <section className="navigation__block">
+  const nav = <>
+    <nav className="navigation navigation__desktop">
+      <section className="navigation__block navigation__block_desktop">
+        <a
+          className="navigation__link"
+          onClick={() => navigate('/movies')}
+        >
+          Фильмы
+        </a>
+        <a
+          className="navigation__link"
+          onClick={() => navigate('/saved-movies')}
+        >
+          Сохранённые фильмы
+        </a>
+      </section>
+      <button
+        className="navigation__button navigation__button_profile"
+        onClick={() => navigate('/profile')}
+        type="button"
+      >
+        Аккаунт
+      </button>
+    </nav>
+
+    <nav className="navigation navigation__mobile">
+      <section className="navigation__block">
+        <div className="navigation__mobile-menu-open"> </div>
+      </section>
+
+      <div className="navigation__mobile-menu-container">
+        <section className="navigation__mobile-block">
+          <button
+            className="navigation__mobile-menu-close"
+            type="button"
+          >
+          </button>
+
+          <div className="navigation__menu">
+            <a
+              className="navigation__link"
+              onClick={() => navigate('/')}
+            >
+              Главная
+            </a>
+
             <a
               className="navigation__link"
               onClick={() => navigate('/movies')}
             >
               Фильмы
             </a>
+
             <a
               className="navigation__link"
               onClick={() => navigate('/saved-movies')}
             >
               Сохранённые фильмы
             </a>
-          </section>
 
-          <button
-            className="navigation__button navigation__button_profile"
-            onClick={() => navigate('/profile')}
-            type="button"
+            <button
+              className="navigation__button navigation__button_profile navigation__button_center"
+              onClick={() => navigate('/profile')}
+              type="button"
             >
-            Аккаунт
-          </button>
-        </nav>
-      }
-      {!loggedin &&
-        <nav className="navigation navigation__register">
-          <section className="navigation__block ">
-            <a
+              Аккаунт
+            </button>
+          </div>
+
+        </section>
+      </div>
+    </nav>
+  </>
+
+  const loginNav = <nav className="navigation">
+    <section className="navigation__block">
+      <a
               className="navigation__link"
               onClick={() => navigate('/signup')}
             >
-              Регистрация
-            </a>
-            <button
-              className="navigation__button navigation__button_blue"
-              onClick={() => navigate('/signin')}
-              type="button"
-            >
-              Войти
-            </button>
-          </section>
-        </nav>
-      }
+      Регистрация
+      </a>
+      <button
+        className="navigation__button navigation__button_blue"
+        onClick={() => navigate('/signin')}
+        type="button"
+      >
+        Войти
+      </button>
+    </section>
+  </nav>
+
+  return (
+    <>
+      {loggedin ? nav : loginNav}
     </>
   )
 }
