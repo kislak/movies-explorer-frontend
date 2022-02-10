017578716576369169eec2e3f97ca0a179a72c9c
serge <kislak7@gmail.com>
2022-02-10 16:14:58 +0300

    fix config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const config = {
     moviesUrl: "https://api.nomoreparties.co/beatfilm-movies",
-    mainUrl: "//localhost:4000/api"
-    // mainUrl: "https://kino.nomoredomains.xyz/api"
+    mainUrl: "https://kino.nomoredomains.xyz/api"
 }
 
 export default config
